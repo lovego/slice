bf31cd0f1bb6614be4137a0c0ee5eb56e22fd3e6
jiayizhao <jiayi.zhao@hztl3.com>
2019-08-05 19:04:57 +0800

    add fullcontains

--- contains.go
+++ contains.go
@@ -26,3 +26,39 @@ func ContainsInt64(slice []int64, target int64) bool {
 	}
 	return false
 }
+
+func FullContainsInt64(slice, target []int64) bool {
+	if len(slice) == 0 && len(target) == 0 {
+		return true
+	}
+
+	left, right := UniqueInt64(slice), UniqueInt64(target)
+	if len(left) != len(right) {
+		return false
+	}
+
+	for _, id := range left {
+		if !ContainsInt64(right, id) {
+			return false
+		}
+	}
+	return true
+}
+
+func FullContainsString(slice, target []string) bool {
+	if len(slice) == 0 && len(target) == 0 {
+		return true
+	}
+
+	left, right := UniqueString(slice), UniqueString(target)
+	if len(left) != len(right) {
+		return false
+	}
+
+	for _, id := range left {
+		if !ContainsString(right, id) {
+			return false
+		}
+	}
+	return true
+}
